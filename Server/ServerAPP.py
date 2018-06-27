fa73e63631a2ec7d8c41d2142130586e93f7e006
Wei <zeus83157@gmail.com>
2018-06-27 14:42:47 +0800

    接收來自Client的資料

--- Server/ServerAPP.py
+++ Server/ServerAPP.py
@@ -2,6 +2,29 @@ import sys
 from PyQt5.QtWidgets import QDialog, QApplication
 from ServerUI import Ui_Form
 import socket
+import threading
+
+class ServerTask (threading.Thread):
+		def __init__(self, ip, port):
+			threading.Thread.__init__(self)
+			self.ip = ip
+			self.port = port
+
+		def run(self):
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.bind((self.ip, self.port))
+			s.listen(5)
+			while True:
+				conn, addr = s.accept()
+
+				while True:
+					data = conn.recv(1024)
+					if (not data): 
+						break
+					data = str(data, encoding = "utf-8")
+					print(data)
+					conn.send(bytes("Hello" + data, encoding = "utf8"))
+		
 
 class AppWindow(QDialog):
 	def __init__(self):
@@ -9,7 +32,26 @@ class AppWindow(QDialog):
 		super().__init__()
 		self.ui = Ui_Form()
 		self.ui.setupUi(self)
+
+		self.ui.IPlineEdit.setText(self.GetIp())
+		self.ui.StartButton.clicked.connect(self.StartButton_Clicked)
+
 		self.show()
+
+	def GetIp(self):
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect(('8.8.8.8', 80))
+			ip = s.getsockname()[0]
+		finally:
+			s.close()
+		return ip
+
+	def StartButton_Clicked(self):
+		ip = self.ui.IPlineEdit.text()
+		port = self.ui.PortlineEdit.text()
+		port = int(port)
+		ServerTask(ip,port).start()
 
 app = QApplication(sys.argv)
 w = AppWindow()
