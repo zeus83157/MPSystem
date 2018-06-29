4d5ba01f806b7f7642a4080c01a0fa7dfe41be29
Wei <zeus83157@gmail.com>
2018-06-30 04:05:41 +0800

    Submit改非同步

--- Client/ClientAPP.py
+++ Client/ClientAPP.py
@@ -2,6 +2,29 @@ import sys
 from PyQt5.QtWidgets import QDialog, QApplication
 from ClientUI import Ui_Form
 import socket
+import threading
+
+
+class ClientTask(threading.Thread):
+	def __init__(self, ip,port,url):
+		threading.Thread.__init__(self)
+		self.ip = ip
+		self.port = port
+		self.url = url
+
+	def run(self):
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((self.ip, self.port))
+			cmd = self.url
+			s.send(bytes(cmd, encoding = "utf8"))
+			data = s.recv(1024)
+			data = str(data, encoding = "utf-8")
+			print (data)
+			s.close()
+		except Exception as e:
+			print(e)
+		
 
 class AppWindow(QDialog):
 	def __init__(self):
@@ -18,17 +41,8 @@ class AppWindow(QDialog):
 		port = int(self.ui.PortlineEdit.text())
 		url = self.ui.UrllineEdit.text()
 
-		try:
-			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			s.connect((ip, port))
-			cmd = url
-			s.send(bytes(cmd, encoding = "utf8"))
-			data = s.recv(1024)
-			data = str(data, encoding = "utf-8")
-			print (data)
-			s.close()
-		except Exception as e:
-			print(e)
+		ctask = ClientTask(ip,port,url)
+		ctask.start()
 
 app = QApplication(sys.argv)
 w = AppWindow()
